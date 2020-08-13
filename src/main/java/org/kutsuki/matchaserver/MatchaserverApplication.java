e9e00b75f70a7109d2255adba8c72dbd5a96c8ae
MatchaGreen <MatchaGreen@192.168.86.88>
2020-08-13 14:32:38 -0400

    Initial Commit

---
+++ src/main/java/org/kutsuki/matchaserver/MatchaserverApplication.java
@@ -0,0 +1,13 @@
+package org.kutsuki.matchaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchaserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/kutsuki/matchaserver/MatchaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kutsuki.matchaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
